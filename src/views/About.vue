ce65cb27d9c918ff6ea563db0bb8e8afa75b861f
Jaspreet Singh <jsprtmnn@gmail.com>
2023-06-22 14:34:26 +0530

    fixed build

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>Todo App Created using VueJS 2</div>
-  <div>Created By: Jaspreet Singh</div>
-  <div><a href="https://github.com/mann1996/todo-app" target="_blank">Github</a></div>
+  <div>
+    <div>Todo App Created using VueJS 2</div>
+    <div>Created By: Jaspreet Singh</div>
+    <div><a class="text-blue-500" href="https://github.com/mann1996/todo-app" target="_blank">Github</a></div>
+  </div>
 </template>
 <script>
 export default {
